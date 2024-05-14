9e4ecc1cc4535192dbe8bdffacaf2f24597047fb
02angejuice <ws.watcharaphong@gmail.com>
2024-05-14 20:50:20 +0700

    bg

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,7 +4,9 @@ import Content from "../components/landing/Content"
 
 export default function LandingPage({ children }) {
   return (
-    <div className="min-h-screen bg-[url('images/image.png')] bg-center bg-cover px-24 py-4">
+    <div
+      className={`min-h-screen bg-[url(images/image.png)] bg-center bg-cover px-24 py-4`}
+    >
       <Navbar />
       <Content />
     </div>
